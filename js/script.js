44d22e04d8b2179c1ccdaa96d1eb8499e29cf0ee
Julián Ordóñez <jm.ord98@gmail.com>
2023-05-01 00:28:34 -0300

    cambios varios 2023

--- js/script.js
+++ js/script.js
@@ -17,3 +17,16 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-list");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}))
